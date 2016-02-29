0ca53d72de90eed7c970bf9f49b00469f2d3d41f
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-02-29 22:14:24 +0200

    Reorder code

--- RadiatorApp.elm
+++ RadiatorApp.elm
@@ -13,8 +13,9 @@ app = StartApp.start { init = (Model.initialModel, Update.refreshBuilds Model.in
 main : Signal Html
 main = app.html
 
+timedUpdate : Signal Model.Action
+timedUpdate = Signal.map (\_ -> Model.RefreshBuilds) (every (30 * second))
+
 port tasks : Signal (Task.Task Never ())
 port tasks = app.tasks
 
-timedUpdate : Signal Model.Action
-timedUpdate = Signal.map (\_ -> Model.RefreshBuilds) (every (30 * second))
