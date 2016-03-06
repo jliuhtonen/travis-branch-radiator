923beb251c8bfcc792cee022a20deb0b72101301
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-03-06 19:18:30 +0200

    Improve readability

--- RadiatorUpdate.elm
+++ RadiatorUpdate.elm
@@ -14,37 +14,46 @@ update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
      RefreshBuilds -> (model, (refreshBuilds model.configuration))
+
      NewBuildStatus (Just builds) -> ((refreshModelBuildState builds model), Effects.none)
+
      NewBuildStatus Nothing -> (model, Effects.none)
+
      FlipConfigMode -> ({ model | mode = (flipAppMode model.mode) }, Effects.none)
      UpdateRepositoryField repository ->
        let cfg = model.configPanel
            configView = { cfg | repositorySlug = repository }
        in ({ model | configPanel = configView }, Effects.none)
+
      AddRepository ->
        let currentConfig = model.configuration
            cfgPanel = model.configPanel
            updatedRepositories = List.append model.configuration.repositories [model.configPanel.repositorySlug]
            updatedModel = ({ model | configuration = { currentConfig | repositories = updatedRepositories }, configPanel = { cfgPanel | repositorySlug = "" } })
        in (updatedModel, refreshBuilds updatedModel.configuration) 
+
      RemoveRepository repository ->
        let currentConfig = model.configuration
            newRepositories = List.filter (\r -> r /= repository) currentConfig.repositories
            updatedConfig = { currentConfig | repositories = newRepositories }
            updatedModel = { model | configuration = updatedConfig }
        in (updatedModel, refreshBuilds updatedConfig)
+
      UpdateApiKeyField key ->
        let cfg = model.configPanel
            configView = { cfg | apiKey = key }
        in ({ model | configPanel = configView }, Effects.none)
+
      SaveApiKey -> 
        let cfg = model.configuration
            cfgPanel = model.configPanel
            updatedCfg = {cfg | apiKey = (Just cfgPanel.apiKey)}
            updatedModel = {model | configuration = updatedCfg}
        in (updatedModel, refreshBuilds updatedModel.configuration)
+
      SaveConfiguration -> ({ model | configuration = model.configuration, 
         mode = Monitoring }, (refreshBuilds model.configuration))
+
 
 refreshModelBuildState: List (String, Travis.BranchStatus) -> Model -> Model 
 refreshModelBuildState updatedBranchStatuses model =
